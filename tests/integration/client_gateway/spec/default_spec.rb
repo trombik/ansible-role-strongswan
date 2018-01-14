e4345b2ef58e878827e570f124caaf69054eed67
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2018-01-14 15:46:49 +0900

    QA

--- tests/integration/client_gateway/spec/default_spec.rb
+++ tests/integration/client_gateway/spec/default_spec.rb
@@ -45,49 +45,47 @@ context "after provisioning finished" do
 end
 
 context "when tunnel has been established" do
-  clients.each do |client|
-    before(:all) do
-      server(client).server.ssh_exec("sudo ipsec down vpn")
-      server(client).server.ssh_exec("sudo ipsec up vpn")
+  before(:all) do
+    server(:client1).server.ssh_exec("sudo ipsec down vpn")
+    server(:client1).server.ssh_exec("sudo ipsec up vpn")
+  end
+
+  after(:all) do
+    server(:client1).server.ssh_exec("sudo ipsec down vpn")
+  end
+
+  client_subnet = %r{172\.16\.0\.11\d\/32}
+  internel_subnet = %r{0\.0\.0\.0\/0}
+
+  describe server(:client1) do
+    it "has an SA" do
+      r = current_server.ssh_exec("sudo ipsec status")
+      expect(r).to match(/Security Associations \(1 up/)
+      expect(r).to match(/vpn\[\d+\]: ESTABLISHED/)
+      # 172.16.0.110/32 === 172.16.0.0/24
+      expect(r).to match(/vpn\{\d+\}:\s+#{client_subnet} === #{internel_subnet}/)
+    end
+  end
+
+  describe server(:server1) do
+    it "has an SA" do
+      r = current_server.ssh_exec("sudo ipsec status")
+      expect(r).to match(/Security Associations \(1 up/)
+      expect(r).to match(/vpn\[\d+\]: ESTABLISHED/)
+      expect(r).to match(/vpn\{\d+\}:\s+#{internel_subnet} === #{client_subnet}/)
+    end
+  end
+
+  describe server(:client1) do
+    it "has default route to WAN via IPSec tunnel" do
+      r = current_server.ssh_exec("route -n get www.example.org")
+      expect(r).to match(/^\s*interface: tun0$/)
     end
 
-    after(:all) do
-      server(client).server.ssh_exec("sudo ipsec down vpn")
-    end
-
-    client_subnet = %r{172\.16\.0\.11\d\/32}
-    internel_subnet = %r{0\.0\.0\.0\/0}
-
-    describe server(:client1) do
-      it "has an SA" do
-        r = current_server.ssh_exec("sudo ipsec status")
-        expect(r).to match(/Security Associations \(1 up/)
-        expect(r).to match(/vpn\[\d+\]: ESTABLISHED/)
-        # 172.16.0.110/32 === 172.16.0.0/24
-        expect(r).to match(/vpn\{\d+\}:\s+#{client_subnet} === #{internel_subnet}/)
-      end
-    end
-
-    describe server(:server1) do
-      it "has an SA" do
-        r = current_server.ssh_exec("sudo ipsec status")
-        expect(r).to match(/Security Associations \(1 up/)
-        expect(r).to match(/vpn\[\d+\]: ESTABLISHED/)
-        expect(r).to match(/vpn\{\d+\}:\s+#{internel_subnet} === #{client_subnet}/)
-      end
-    end
-
-    describe server(client) do
-      it "has default route to WAN via IPSec tunnel" do
-        r = current_server.ssh_exec("route -n get www.example.org")
-        expect(r).to match(/^\s*interface: tun0$/)
-      end
-
-      it "gets HTTP resource from external host" do
-        # ICMP is preferred, but virtualbox does not NAT ICMP, use HTTP instead
-        r = current_server.ssh_exec("curl -sI http://www.example.org/")
-        expect(r).to match(%r{^HTTP/1\.1 200 OK})
-      end
+    it "gets HTTP resource from external host" do
+      # ICMP is preferred, but virtualbox does not NAT ICMP, use HTTP instead
+      r = current_server.ssh_exec("curl -sI http://www.example.org/")
+      expect(r).to match(%r{^HTTP/1\.1 200 OK})
     end
   end
 end
